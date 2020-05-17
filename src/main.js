385226307d8d946416c5158031c2f306311fe432
Thach Tran <rizzaxc@gmail.com>
2020-05-18 09:14:10 +1000

    TODO in main.js

--- src/main.js
+++ src/main.js
@@ -8,3 +8,15 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+/** 
+ * TODO:
+ *  Fix History jumping up when Result empty
+ *  Move the buttons to the right of Result in a square
+ *  Add indications when multiple kanjis are returned
+ *  Add small Delete button to each KanjiCard; when pressed the card's removed
+ *  WK Level OverView
+ *  Attach Logo
+ *  Move the search bar down a bit
+ */
